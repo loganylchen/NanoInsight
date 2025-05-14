cac3cae78fb221d3eb8779682cb138705f42705a
loganylchen <yuelong.chen.btr@gmail.com>
2025-05-14 10:50:29 +0800

    refactor(prep_raw_fastq.sh): improve readability and add debug lines
    Update awk syntax for better readability and add debug lines to inspect input and output fastq files

--- workflow/scripts/prep_raw_fastq.sh
+++ workflow/scripts/prep_raw_fastq.sh
@@ -12,11 +12,12 @@ output_fastq="${snakemake_output[fastq]}"
 
 
 
-zcat ${input_fastq} | awk -F " " "{print $1}" | gzip -c > ${output_fastq} 
+zcat ${input_fastq} | awk -F ' ' '{print $1}' | gzip -c > ${output_fastq} 
 zcat ${input_fastq} | wc -l 
+zcat ${input_fastq} | head -n 4
 echo `date`
 zcat ${output_fastq}  | wc -l 
-
+zcat ${output_fastq}  | head -n 4
 
 
 
